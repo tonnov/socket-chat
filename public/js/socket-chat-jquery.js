a2db418032c4c119a7f2a8fac3f51d7b82c5de44
tonov <nogueda@gmail.com>
2020-05-06 04:59:46 +0000

    add chat functionality

--- public/js/socket-chat-jquery.js
+++ public/js/socket-chat-jquery.js
@@ -1,8 +1,15 @@
 
 var params = new URLSearchParams(window.location.search);
 
+var nombre = params.get('nombre');
+var sala = params.get('sala');
+
 // referencias de jQuery
 var divUsuarios = $('#divUsuarios');
+var formEnviar = $('#formEnviar');
+var txtMensaje = $('#txtMensaje');
+var divChatbox = $('#divChatbox');
+
 
 // Funciones para renderizar usuarios
 
@@ -24,3 +31,97 @@ function renderizarUsuarios(personas){
 
     divUsuarios.html(html);
 }
+
+function renderizarMensajes(mensaje, yo) {
+
+    var html = '';
+    var fecha = new Date(mensaje.fecha);
+    var hora = fecha.getHours() + ';' + fecha.getMinutes();
+
+    var adminClass = 'info';
+
+    if (mensaje.nombre === 'Administrador') {
+        adminClass = 'danger';
+    }
+
+
+    if (yo) {
+
+        html += '<li class="reverse">';
+        html += '    <div class="chat-content">';
+        html += '        <h5>' + mensaje.nombre + '</h5>';
+        html += '        <div class="box bg-light-inverse">' + mensaje.mensaje + '</div>';
+        html += '    </div>';
+        html += '    <div class="chat-img"><img src="assets/images/users/5.jpg" alt="user" /></div>';
+        html += '    <div class="chat-time">' + hora + '</div>';
+        html += '</li>';
+        
+    } else {
+        
+        html += '<li>';
+        if (mensaje.nombre != 'Administrador') {
+            html +=     '<div class="chat-img"><img src="assets/images/users/1.jpg" alt="user" /></div>';
+        }
+        html +=     '<div class="chat-content">';
+        html +=         '<h5>'+ mensaje.nombre + '</h5>';
+        html +=         '<div class="box bg-light-' + adminClass + '">' + mensaje.mensaje + '</div>';
+        html +=     '</div>';
+        html +=     '<div class="chat-time">' + hora + '</div>';
+        html += '</li>';
+
+    }
+
+    divChatbox.append(html);
+}
+
+function scrollBottom() {
+
+    // selectors
+    var newMessage = divChatbox.children('li:last-child');
+
+    // heights
+    var clientHeight = divChatbox.prop('clientHeight');
+    var scrollTop = divChatbox.prop('scrollTop');
+    var scrollHeight = divChatbox.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight() || 0;
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        divChatbox.scrollTop(scrollHeight);
+    }
+}
+
+
+// Listeners
+
+divUsuarios.on('click', 'a', function(){
+    
+    var id = $(this).data('id');
+
+    if (id) {
+        
+        console.log('el id clickado es: ' + id);
+
+    }
+    
+});
+
+formEnviar.on('submit', function(e){
+    
+    e.preventDefault();
+
+    if (txtMensaje.val().trim().length === 0) {
+        return;
+    }
+
+    socket.emit('crearMensaje', {
+        nombre: nombre,
+        mensaje: txtMensaje.val()
+    }, function(mensaje) {
+        txtMensaje.val('').focus();
+        renderizarMensajes(mensaje, true);
+        scrollBottom();
+    });
+    //console.log(txtMensaje.val());
+    
+})
--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -27,18 +27,22 @@ io.on('connection', (client) => {
 
         client.broadcast.to(data.sala).emit('listaPersona', usuarios.getPersonasPorSala(data.sala));
 
+        client.broadcast.to(data.sala).emit('crearMensaje', crearMensaje('Administrador', `${data.nombre} se unió`));
+
         callback(usuarios.getPersonasPorSala(data.sala));
         
         //console.log(personas);
         
     });
 
-    client.on('crearMensaje', (data) => {
+    client.on('crearMensaje', (data, callback) => {
         let persona = usuarios.getPersona(client.id);
         // console.log(persona);
         
         let mensaje = crearMensaje(persona.nombre, data.mensaje);
         client.broadcast.to(persona.sala).emit('crearMensaje', mensaje);
+
+        callback(mensaje);
     });
 
     client.on('disconnect', () => {
